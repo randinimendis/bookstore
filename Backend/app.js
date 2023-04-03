045105da6ea34da130139789c86b8feab39fef5d
Randini Mendis <99355199+randinimendis@users.noreply.github.com>
2023-04-03 15:48:31 +0530

    initial commit

---
+++ Backend/app.js
@@ -0,0 +1,17 @@
+
+const express = require('express')
+const mongoose = require('mongoose')
+const router = require('./routes/book-routes')
+const route=require("./routes/book-routes")
+
+const app = express()
+//meddlewares
+app.use(express.json());
+app.use("/books",router)//locallhost:5000/books
+
+mongoose
+  .connect("mongodb+srv://randini:oEWkXnZbCA14qMSS@randini.hxibu1x.mongodb.net/bookstore?retryWrites=true&w=majority")
+  .then(() => console.log("Connected to database"))
+  .then(() =>{app.listen(5000);
+})
+.catch((err)=> console.log(err));
---
+++ Backend/routes/book-routes.js
@@ -0,0 +1,13 @@
+//const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
+const Book = require("../model/Book");
+const booksController=require("../controllers/book-controller")
+
+router.get("/", booksController.getAllBooks);
+router.post("/",booksController.addBook);
+router.get("/:id",booksController.getById);
+router.put("/:id",booksController.updateBook);
+router.delete("/:id",booksController.deleteBook);
+
+module.exports=router;
