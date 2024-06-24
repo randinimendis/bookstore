05e52f90c799f88017a46c527ce87e4fdd8833d9
Randini Mendis <99355199+randinimendis@users.noreply.github.com>
2024-06-24 09:58:15 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -5,12 +5,11 @@ const router = require('./routes/book-routes')
 const route=require("./routes/book-routes")
 
 const app = express()
-//meddlewares
+//meddlewares 
 app.use(express.json());
 app.use("/books",router)//locallhost:5000/books
 
-mongoose
-  .connect("mongodb+srv://randini:oEWkXnZbCA14qMSS@randini.hxibu1x.mongodb.net/bookstore?retryWrites=true&w=majority")
+
   .then(() => console.log("Connected to database"))
   .then(() =>{app.listen(5000);
 })
